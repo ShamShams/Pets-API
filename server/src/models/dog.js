21f3d90c947391e6b01a825dc26db5d23e83565e
Shamshams <shamila.remtoula@hotmail.fr>
2017-10-29 01:51:14 +0200

    Add dog model

---
+++ server/src/models/dog.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const dogSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  age: { type: Number, required: true },
+  race: { type: String, required: true },
+  gentil: { type: Boolean, required: true },
+  calme: { type: Boolean, required: true },
+  joueur: { type: Boolean, required: true },
+  affectueux: { type: Boolean, required: true },
+  inteligent: { type: Boolean, required: true },
+  propre: { type: Boolean, required: true }
+}, { versionKey: false });
+
+export defaut mongoose.model('Dog', dogSchema);
